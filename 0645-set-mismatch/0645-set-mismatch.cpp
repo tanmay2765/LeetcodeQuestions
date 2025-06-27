b1f32d4486adb2454801dbd295dad7ae419f8296
tanmay2765 <149183175+tanmay2765@users.noreply.github.com>
2025-06-27 10:51:27 +0530

    Time: 4 ms (42.81%), Space: 25.2 MB (69.4%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& a) {
+        int i=0,n=a.size();
+        while(i<n){
+            int c=a[i]-1;
+            if(a[i]!=a[c]){
+                swap(a[i],a[c]);
+            }else i++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (a[i] != i + 1) {
+                return {a[i], i + 1};}
+        }
+        return {-1,-1};
+    }
+};
