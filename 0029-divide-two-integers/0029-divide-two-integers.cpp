c680460bfea5654f41162a79a9b0bd8073bdb57b
tanmay2765 <149183175+tanmay2765@users.noreply.github.com>
2025-06-27 10:46:18 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.5%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long divide(long long d1,long long d2) {
+        if (d1 == INT_MIN && d2 == -1)
+            return INT_MAX;
+        long long res=abs(d1)/abs(d2);
+        if((d1<0 && d2>0) || (d1>0 && d2<0)){
+            return -res;
+        }else return res;
+    }
+};
