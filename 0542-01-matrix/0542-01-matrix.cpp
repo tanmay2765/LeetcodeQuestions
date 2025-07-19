a3cec3a1fcbd621d65dd8af62a527a02d7619844
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2025-07-19 15:31:27 +0530

    Time: 22 ms (47.59%), Space: 34.6 MB (93.96%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int r = mat.size(), c = mat[0].size();
+        int fresh = 0, rotten = 0;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (mat[i][j] == 0) {
+                    q.push({i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        int dx[4] = { 0  , 0 , 1 , -1 };
+        int dy[4] = { 1 , -1 , 0 , 0 };
+        int min = 1;
+
+        while(q.size() > 0)
+        {
+            int rot_at_single_minute = q.size();
+
+            for(int i = 0; i < rot_at_single_minute; i++)
+            {
+                pair<int,int> temp = q.front();
+                q.pop();
+                for(int j = 0; j < 4; j++)
+                {
+                    int x = temp.first + dx[j];
+                    int y = temp.second + dy[j];
+
+                    if( x >= 0 && y >= 0 && x < r && y < c && mat[x][y] == -1)
+                    {    
+                        mat[x][y] = min;  
+                        q.push({x,y});
+                    }
+                }
+            }
+            if(q.size() > 0) min++;
+        }
+
+        return mat;  
+    }
+};
