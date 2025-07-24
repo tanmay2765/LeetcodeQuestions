8bac608d285ed998b2c607ac35dbca021ddec0ca
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2025-07-24 12:04:27 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.29%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,14 +4,9 @@ public:
         int l=0,r=nums.size()-1;
         while(l<=r){
             int m=(l+r)/2;
-            if(t==nums[m]){
-                return m;
-            }
-            else if(nums[m]<t){
-                l=m+1;
-            }else{
-                r=m-1;
-            }
+            if(t==nums[m]) return m;
+            else if(t<nums[m]) r=m-1;
+            else l=m+1;
         }
         return -1;
     }
