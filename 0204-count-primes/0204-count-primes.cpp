52d38abc3c66bf16593f9a0a1c072b30b5b2e844
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2025-12-24 21:11:05 +0530

    Time: 147 ms (89.28%), Space: 132.6 MB (7.53%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        int c=0;
-        if (n <= 2) {
-            return 0;
-        }
-        vector<bool> pr(n,true);
+        if(n<2) return 0;
+        vector<int> pr(n,true);
         pr[0]=pr[1]=false;
         for(int i=2;i*i<n;i++){
             if(pr[i]){
-                for(int j=i*i;j<n;j+=i){
-                    pr[j]=false;
-                }
+                for(int j=i*i;j<n;j+=i) pr[j]=false;
             }
         }
-        for(int i=2;i<n;i++){
-            if(pr[i]) c++;
+        int c=0;
+        for(bool p:pr){
+            if(p) c++;
         }
         return c;
     }
