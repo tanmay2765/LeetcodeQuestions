0ced98c9a19732f077ea2c3177dd95462cc724f1
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-21 14:10:25 +0530

    Time: 0 ms (100%), Space: 20.7 MB (57.6%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,8 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
         int res=nums[0];
         for(int i=1;i<nums.size();i++){
-            res^=nums[i];}
-
-       return res;
+            res^=nums[i];
+        }
+        return res;
     }
 };
