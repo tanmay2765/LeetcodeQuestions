89ff49d963d4e8a63941c7c50c3dd8d8506f4c42
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-21 10:49:46 +0530

    Time: 47 ms (23.49%), Space: 125.6 MB (64.76%) - LeetHub

--- 3618-split-array-by-prime-indices/3618-split-array-by-prime-indices.cpp
+++ 3618-split-array-by-prime-indices/3618-split-array-by-prime-indices.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    bool prime(int a){
-        if(a<=1){ 
-            return false;
-        }
-        for(int i=2;i*i<=a;i++){
-            if(a%i==0) return false;
+    bool prime(int n){
+        if(n<2) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
         }
         return true;
     }
-        
     long long splitArray(vector<int>& nums) {
-        vector<int> suma;
-        vector<int> sumb;
-        long long a=0,b=0;
-        for(int i=0;i<nums.size();i++){
-            if(prime(i)) suma.push_back(nums[i]);
-            else sumb.push_back(nums[i]);
+        int n=nums.size();
+        long long s1=0,s2=0;
+        for(int i=0;i<n;i++){
+            if(prime(i)) s1+=nums[i];
+            else s2+=nums[i];
         }
-        for(int i=0;i<suma.size();i++){
-            a+=suma[i];
-        }
-        for(int i=0;i<sumb.size();i++){
-            b+=sumb[i];
-        }
-        return llabs(a-b);
+        return llabs(s1-s2);
     }
 };
