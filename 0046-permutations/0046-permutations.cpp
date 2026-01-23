aff67426ecfd2645bd204d13b7c691dc1b8766b9
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-23 13:07:57 +0530

    Time: 0 ms (100%), Space: 10.5 MB (87.36%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void fun(vector<vector<int>>& res,vector<int> nums,vector<int> temp,vector<bool> used){
+    void fun(vector<vector<int>> &res,vector<int>& temp,vector<int>& nums,vector<bool>& used){
         if(nums.size()==temp.size()) res.push_back(temp);
         for(int i=0;i<nums.size();i++){
             if(used[i]==true) continue;
             temp.push_back(nums[i]);
             used[i]=true;
-            fun(res,nums,temp,used);
+            fun(res,temp,nums,used);
             temp.pop_back();
             used[i]=false;
         }
@@ -15,7 +15,7 @@ public:
         vector<vector<int>> res;
         vector<int> temp;
         vector<bool> used(nums.size(),false);
-        fun(res,nums,temp,used);
+        fun(res,temp,nums,used);
         return res;
     }
 };
