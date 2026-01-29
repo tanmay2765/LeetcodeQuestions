08c7b4c8c8394246799948db8f1beda0faaf704d
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-29 16:43:49 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int k = 0; // position for next non-zero
-        
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != 0) {
-                nums[k] = nums[i];
-                k++;
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[k++]=nums[i];
             }
-        }
-        
-        while(k < nums.size()) {
-            nums[k] = 0;
-            k++;
+        }while(k<nums.size()){
+            nums[k++]=0;
         }
     }
 };
