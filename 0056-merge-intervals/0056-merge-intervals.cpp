42ab7549efda928a69f73d02c7fd022918195e2b
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-30 11:46:48 +0530

    Time: 4 ms (66.81%), Space: 23.7 MB (80.53%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> merge;
         merge.push_back(inter[0]);
         for(int i=1;i<inter.size();i++){
-            if(merge.back()[1]>=inter[i][0]) merge.back()[1]=max(merge.back()[1],inter[i][1]);
+            if(inter[i][0]<=merge.back()[1]) merge.back()[1]=max(inter[i][1],merge.back()[1]);
             else merge.push_back(inter[i]);
         }
         return merge;
