8e7cf91d6c2980f434e2415ae8b56e431e00e482
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-30 09:19:15 +0530

    Time: 0 ms (100%), Space: 11.6 MB (77.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,15 +4,9 @@ public:
         int n=nums.size();
         int l=0,m=0,h=n-1;
         while(h>=m){
-            if(nums[m]==0){
-                swap(nums[l],nums[m]);
-                l++;
-                m++;
-            }else if(nums[m]==1) m++;
-            else{
-                swap(nums[m],nums[h]);
-                h--;
-            }
+            if(nums[m]==0) swap(nums[l++],nums[m++]);
+            else if(nums[m]==1) m++;
+            else swap(nums[m],nums[h--]);
         }
     }
 };
