2a48b3fbf2945a2778c680e0d057d3829aa4c13b
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-31 15:51:53 +0530

    Time: 12 ms (27.46%), Space: 22.9 MB (44.77%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    bool fun(int m, vector<int>& p, int h) {
-        long long hr = 0;
-        for (int i = 0; i < p.size(); i++) {
-            hr += ceil((p[i]*1.0)/m);
-        }
-        return hr <= h;
+
+    bool fun(vector<int>& piles,int m, int h){
+        long long hr=0;
+        for(int i=0;i<piles.size();i++){
+            hr+=ceil((piles[i]*1.0)/m);
+        }return hr<=h;
     }
 
-    int minEatingSpeed(vector<int>& p, int h) {
-        int l = 1, r = 1e9, ans;
-        while (l <= r) {
-            int m = (l + r ) / 2; 
-            if (fun(m, p, h)) {
-                ans = m;
-                r = m - 1;
-            } else {
-                l = m + 1;
-            }
-        }
-        return ans;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1,r=1e9,ans;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(fun(piles,m,h)){
+                ans=m;
+                r=m-1;
+            }else l=m+1;
+        }return ans;
     }
 };
