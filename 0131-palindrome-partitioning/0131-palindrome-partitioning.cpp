9981a258cc20a45510241d6ae74d29df95a7a469
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-31 10:42:41 +0530

    Time: 59 ms (40.29%), Space: 61.6 MB (49.12%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    bool pal(const string& s, int l, int h) {
+    bool pal(string s,int l,int h){
         while(l<h){
             if(s[l]!=s[h]) return false;
-                l++;
-                h--;
-            
-        }
-        return true;
+            l++;
+            h--;
+        }return true;
     }
-
-    void fun(const string& s, vector<vector<string>>& res, vector<string>& temp, int start) {
-        if (start == s.size()) {
-            res.push_back(temp);
+    void fun(string s,vector<vector<string>>&res,vector<string>& temp,int start){
+        if(start==s.size()) {
+            res.push_back(temp); 
             return;
         }
-
-        for (int i = start; i < s.size(); i++) {
-            if (pal(s, start, i)) {
-                temp.push_back(s.substr(start, i - start + 1));
-                fun(s, res, temp, i + 1);
+        for(int i=start;i<s.size();i++){
+            if(pal(s,start,i)){
+                temp.push_back(s.substr(start,i-start+1));
+                fun(s,res,temp,i+1);
                 temp.pop_back();
             }
         }
@@ -28,7 +24,7 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> temp;
-        fun(s, res, temp, 0);
+        fun(s,res,temp,0);
         return res;
     }
 };
