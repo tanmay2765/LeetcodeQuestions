06771edd8a9a7ea82b45b189c90cf9d1dd7d9679
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-02-02 14:18:47 +0530

    Time: 2 ms (50.61%), Space: 10.4 MB (91.1%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int open = 0, close = 0, maxLen = 0;
-        
-        // Left to right
-        for(char ch : s) {
-            if(ch == '(') open++;
+        int open=0,close=0,mx=0;
+        for(char c:s){
+            if(c=='(') open++;
             else close++;
-            
-            if(open == close) {
-                maxLen = max(maxLen, 2 * close);
-            } else if(close > open) {
-                open = close = 0;
-            }
-        }
-        
-        open = close = 0;
-        
-        // Right to left
-        for(int i = s.size() - 1; i >= 0; i--) {
-            if(s[i] == '(') open++;
+            if(open ==close) mx=max(mx,2*close);
+            else if(close>open) open=close=0;
+        }open=close=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='(') open++;
             else close++;
-            
-            if(open == close) {
-                maxLen = max(maxLen, 2 * open);
-            } else if(open > close) {
-                open = close = 0;
-            }
-        }
-        
-        return maxLen;
+            if(open==close) mx=max(mx,2*close);
+            else if(open>close) open=close=0;
+        }return mx;
     }
 };
