3b48525667b88421c25d324ca1a8d8a474f56da5
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-02-02 09:39:47 +0530

    Time: 16 ms (20.33%), Space: 35.2 MB (18.97%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     bool sum(vector<int>& w, int d,int m){
-        int i=0,c=0,d2=1;;
+        int i=0,c=0,d2=0;
         while(i<w.size()){
             if(c+w[i]>m){
                 d2++;
                 c=0;
             }else{
-                c+=w[i];
-                i++;
+                c+=w[i++];
             }
-        }
-        return d2<=d;
+        }return d2+1<=d;
     }
+
     int shipWithinDays(vector<int>& w, int d) {
         int l=0,r=0;
         for(int i=0;i<w.size();i++){
-                r+=w[i];
-                l=max(l,w[i]);
-        }
-        while(l<r){
+            r+=w[i];
+            l=max(l,w[i]);
+        }while(l<r){
             int m=(l+r)/2;
-            bool a=sum(w,d,m);
-            if(a){
-                r=m;
-            }else l=m+1;
-        }
-        return l;
+            if(sum(w,d,m)) r=m;
+            else l=m+1;
+        }return l;
     }
 };
