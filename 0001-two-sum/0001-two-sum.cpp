a0cfc4456a64ccb0b705552bccfd682e9b2f05ba
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-02-04 00:40:08 +0530

    Time: 0 ms (100%), Space: 15 MB (19.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,9 +4,8 @@ public:
         unordered_map<int,int> map;
         for(int i=0;i<nums.size();i++){
             int comp=target-nums[i];
-            if(map.find(comp)!=map.end()) return {map[comp],i};
+            if(map.find(comp)!=map.end()) return{map[comp],i};
             map[nums[i]]=i;
-        }
-        return {};
+        }return {};
     }
 };
