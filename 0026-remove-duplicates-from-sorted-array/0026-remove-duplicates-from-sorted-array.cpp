8965cd08ec8ad378562c3bb7f323682bec4ef79c
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-02-04 00:44:19 +0530

    Time: 0 ms (100%), Space: 22.5 MB (79.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,10 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i=0;
         for(int j=1;j<nums.size();j++){
-            if(nums[j]!=nums[i]){
-                i++;
-                nums[i]=nums[j];
-            }
+            if(nums[j]!=nums[i]) nums[++i]=nums[j];
         }
         return i+1;
     }
