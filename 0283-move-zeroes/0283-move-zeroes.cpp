e2adf8190ca5e4f7d6f4aca3844cf74881fbd775
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-02-04 00:30:59 +0530

    Time: 0 ms (100%), Space: 24 MB (8.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,11 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int k=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                nums[k++]=nums[i];
-            }
-        }while(k<nums.size()){
-            nums[k++]=0;
-        }
+            if(nums[i]!=0) nums[k++]=nums[i];
+        }while(k<nums.size()) nums[k++]=0;
     }
 };
