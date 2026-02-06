de80dec81ee00ac525acacdb04b94e5a93792f29
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-02-06 13:07:01 +0530

    Time: 1 ms (11.46%), Space: 15.4 MB (23.77%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -11,19 +11,16 @@
  */
 class Solution {
 public:
-    void check(TreeNode* root,vector<int>& ar){
+    vector<int> arr1,arr2;
+    void fun(TreeNode* root,vector<int> &arr){
         if(!root) return;
-        check(root->left,ar);
-        check(root->right,ar);
-        if (!root->left && !root->right) {
-            ar.push_back(root->val);
-            return;
-        }
+        if(!root->left && !root->right) arr.push_back(root->val);
+        fun(root->left,arr);
+        fun(root->right,arr);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> arr1,arr2;
-        check(root1,arr1);
-        check(root2,arr2);
+        fun(root1,arr1);
+        fun(root2,arr2);
         return arr1==arr2;
     }
 };
