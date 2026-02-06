d6b4990cb2a4d3f3e5003241c4a3b049c2e7053e
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-02-06 13:36:30 +0530

    Time: 29 ms (42.88%), Space: 16.1 MB (89.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j, int r, int c) {
-        if (i < 0 || j < 0 || i >= r || j >= c || grid[i][j] != '1') return;
-        grid[i][j] = '0';  // Mark as visited
-        dfs(grid, i + 1, j, r, c);
-        dfs(grid, i - 1, j, r, c);
-        dfs(grid, i, j + 1, r, c);
-        dfs(grid, i, j - 1, r, c);
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i<0 || j<0 ||i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
-
     int numIslands(vector<vector<char>>& grid) {
-        int r = grid.size(), c = grid[0].size(), ans = 0;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j, r, c);
+        int ans=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
                     ans++;
                 }
             }
-        }
-        return ans;
+        }return ans;
     }
 };
